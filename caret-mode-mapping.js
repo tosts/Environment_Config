76c0ac6ce62fba15bf3c1f30bacd77be1a75e1e8
Simon <t-sim@gmx.net>
2015-01-13 22:41:32 +0100

    Vimperator mapping js plugin (Workaround)

---
+++ caret-mode-mapping.js
@@ -0,0 +1,28 @@
+function exoticModeMap (lhs, rhs, optionalModes) {
+  let noremap = true,
+      silent = true,
+      urls = void 0,
+      specifiedModes = optionalModes || [modes.CARET, modes.HINTS, modes.EMBED, modes.CUSTOM, modes.EX, modes.PROMPT];
+
+  rhs = mappings._expandLeader(rhs);
+
+  mappings.addUserMap(
+    specifiedModes,
+    //[modes.INSERT],
+    [lhs],
+    "User defined mapping",
+    function (count) {
+      events.feedkeys((count || "") + rhs, noremap, silent);
+    },
+    {
+        count: true,
+        rhs: events.canonicalKeys(rhs),
+        noremap: !!noremap,
+        silent: silent,
+        matchingUrls: urls
+    }
+  );
+}
+
+exoticModeMap('jk', '<Esc>') // not working for hint mode.... yet
+exoticModeMap('jk', '<Esc>', [modes.INSERT, modes.TEXTAREA])
