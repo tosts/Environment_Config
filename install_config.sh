36abb713f516da21cba144f5fdd6dc3d5e18fddf
Simon Tost <simon.tost@tngtech.com>
2015-04-24 01:15:09 +0200

    including a dummy gitconfig for aliases

--- install_config.sh
+++ install_config.sh
@@ -4,6 +4,12 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+if [ ! -f ~/.gitconfig ]; then
+    cp gitconfig ~/.gitconfig
+fi
+
 for file in *rc; do
-    ln -fs Environment_Config/$file ../\.$file
+    if [ ! -f ~/\.$file ]; then
+        ln -s Environment_Config/$file ../\.$file
+    fi
 done
