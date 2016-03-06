b8f62abd453b2a3d4af47ec8e82367a983d71bc7
xxx <x@y.z>
2016-03-06 17:58:23 +0000

    Undo and swap folder for vim setup, link aliases file

--- install_config.sh
+++ install_config.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+set -x
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+for folder in \
+  ~/.vim/vimswp \
+  ~/.vim/vimundo \
+; do
+    if [ ! -d $folder ]; then
+      mkdir -p $folder
+    fi
+done
+
 if [ ! -f ~/.gitconfig ]; then
     cp gitconfig ~/.gitconfig
 fi
 
-for file in *rc; do
+for file in *rc bash_aliases; do
     if [ ! -f ~/\.$file ]; then
         ln -s Environment_Config/$file ../\.$file
     fi
